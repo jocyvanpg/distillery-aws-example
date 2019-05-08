1e4c105a12f429b210391946fce318070ddfb187
jocyvan <jocyvancastro@pag.ai>
2019-05-08 11:32:30 -0300

    Fixed syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ config :distillery_example, ExampleWeb.Endpoint,
   version: Application.spec(:distillery_example, :vsn)
 
 config :distillery_example, Example.Repo,
-  adapter: Ecto.Adapters.Postgres
+  adapter: Ecto.Adapters.Postgres,
   pool_size: 10
 
 config :logger,
